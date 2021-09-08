d97e3e8f4b406799db619296c151e5c4af4b73c6
Loïc Bogaert <loicbogaertvauban@gmail.com>
2021-09-08 21:13:01 +0200

    fix portfolio txt

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -15,28 +15,28 @@ const Portfolio = () => {
             <a className="portfolioContainer__link" href="https://loicbogaert.github.io/Creer_site_e-commerce" target="_blank" rel="noreferrer">
                 <div className="portfolioContainer__item">
                 <img src={orinocco} alt="aperçu du site"/>
-                <p className="portfolioContainer__txt">Orinoco : Le but de ce projet était de créer le front-end d'un site d'e-commerce et de communiquer avec une API qui avait été fournie à l'avance.</p>
+                <p className="portfolioContainer__txt" id="txt">Orinoco : Le but de ce projet était de créer le front-end d'un site d'e-commerce et de communiquer avec une API qui avait été fournie à l'avance.</p>
                 </div>
             </a>
 
             <a className="portfolioContainer__link" href="https://loicbogaert.github.io/Dynamiser_avec_animations_css/" target="_blank" rel="noreferrer">
                 <div className="portfolioContainer__item">
                     <img src={ohmyfood} alt="aperçu du site"/>
-                    <p className="portfolioContainer__txt">OhMyFood : mise en forme d'une maquette pour un site de restauration avec html et sass</p>
+                    <p className="portfolioContainer__txt" id="txt">OhMyFood : mise en forme d'une maquette pour un site de restauration avec html et sass</p>
                 </div>
             </a>
 
             <a className="portfolioContainer__link" href="https://github.com/loicbogaert/Construire_une_API_securisee-Back-" target="_blank" rel="noreferrer">
                 <div className="portfolioContainer__item">
                 <img src={piquante} alt="aperçu du site"/>
-                <p className="portfolioContainer__txt">Piquante : Le but du projet était de créer une API sécurisée pour une application d'avis gastronomique avec un front-end déjà existant.</p>
+                <p className="portfolioContainer__txt" id="txt">Piquante : Le but du projet était de créer une API sécurisée pour une application d'avis gastronomique avec un front-end déjà existant.</p>
                 </div>
             </a>
 
             <a className="portfolioContainer__link" href="https://loicbogaert.github.io/Optimiser_un_site_web/" target="_blank" rel="noreferrer">
                 <div className="portfolioContainer__item">
                     <img src={chouette} alt="aperçu du site"/>
-                    <p className="portfolioContainer__txt">La Chouette Agence : Le projet consistait à améliorer les performances d'un site déjà existant (
+                    <p className="portfolioContainer__txt" id="txt">La Chouette Agence : Le projet consistait à améliorer les performances d'un site déjà existant (
                     SEO, accessibilité, performance)
                     </p>
                 </div>
@@ -46,7 +46,7 @@ const Portfolio = () => {
             <a className="portfolioContainer__link">
                 <div className="portfolioContainer__item">
                     <img src="img/ohmyfood.png" alt="aperçu du site"/>
-                    <p className="portfolioContainer__txt">Réalisation d'un réseau social d'entreprise : Backend (API, BDD) + front-end. Comprends la création de compte, 
+                    <p className="portfolioContainer__txt" id="txt">Réalisation d'un réseau social d'entreprise : Backend (API, BDD) + front-end. Comprends la création de compte, 
                         envoi de messages, création d'articles, modération...
                     </p>
                 </div>
@@ -55,7 +55,7 @@ const Portfolio = () => {
             <a className="portfolioContainer__link">
                 <div className="portfolioContainer__item">
                     <img src="" alt="aperçu du site"/>
-                    <p className="portfolioContainer__txt">Projet en cours ...</p>
+                    <p className="portfolioContainer__txt" id="txt">Projet en cours ...</p>
                 </div>
             </a>
         </div>
